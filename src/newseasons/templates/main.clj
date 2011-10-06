29b6d9d5cfaab637062653ee7b9267bebdd414d3
Steve Losh <steve@stevelosh.com>
2011-10-05 22:53:06 -0400

    Fucking GA.

--- src/newseasons/templates/main.clj
+++ src/newseasons/templates/main.clj
@@ -6,6 +6,21 @@
     hiccup.form-helpers))
 
 
+(def ga "<script type=\"text/javascript\">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-15328874-7']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>")
+
+
 ; Utils -----------------------------------------------------------------------
 (defn include-less [href]
   [:link {:rel "stylesheet/less" :type "text/css" :href href}])
@@ -44,7 +59,8 @@
         (link-to "http://stevelosh.com/" "Steve Losh")
         " with "
         (link-to "http://webnoir.org/" "Noir")
-        "."]]]]))
+        "."]]]
+     ga]))
 
 (defpartial inner [title body-class & content]
   (base body-class
